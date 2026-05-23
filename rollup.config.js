46ffdc8022bb88571bc1ae186125725a300696b0
deepikagandla7456 <deepikagandla07@gmail.com>
2026-05-23 22:14:35 +0530

    Package UI-Verse as a TypeScript-based web-component library and add automated publish pipeline

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' })
+  ],
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    }
+  ]
+};
